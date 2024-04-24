ea6b8056113016c088476b6bbcfd0b8645dee169
Uzair Sayyed <sayeduzair54321@gmail.com>
2024-04-24 18:47:20 +0530

    changes

--- frontend/src/components/landingpage/HeroBanner.js
+++ frontend/src/components/landingpage/HeroBanner.js
@@ -1,18 +1,34 @@
 import React from "react";
 import heroBannerImg from "../../assets/students.svg";
+import { Link } from "react-router-dom";
 
 function HeroBanner() {
   return (
     <section className='heroBanner-container'>
       <div className='heroBanner-wrapper'>
         <div className='heroBanner-left'>
-          <img src={ heroBannerImg } />
+          <img src={heroBannerImg} />
         </div>
         <div className='heroBanner-right'>
           <h1>
             Student Report <span>Advisor</span>
           </h1>
-          <p>Student Report Advisor is here to help with your academic journey. Elevate your grade with proper guidance, polished reports and Academic Excellence with the functionality "A Report with Recommendation".</p>
+          <p>
+            Student Report Advisor is here to help with your academic journey.
+            Elevate your grade with proper guidance, polished reports and
+            Academic Excellence with the functionality "A Report with
+            Recommendation".
+          </p>
+
+          <div className='heroBanner-logins'>
+            <button>
+              <Link to='/choose'>Login</Link>
+            </button>
+
+            <p>
+              Don't have an account? <Link to='/Adminregister'>Sign up</Link>
+            </p>
+          </div>
         </div>
       </div>
     </section>
