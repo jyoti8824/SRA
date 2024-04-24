a581e6a2c86983a71c59920fd28980d5d79a0836
Uzair Sayyed <sayeduzair54321@gmail.com>
2024-04-24 23:10:15 +0530

    added cards in landing page

--- frontend/src/components/landingpage/About.js
+++ frontend/src/components/landingpage/About.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Accordian from './Accordian';
 import "./styles.css";
+import AboutCard from './AboutCard';
 
 function About() {
     return (
         <section className='about-container'>
+            <AboutCard/>
             <Accordian />
+
         </section>
     );
 }
