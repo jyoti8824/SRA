a79ff00d9714e93aed7b71fde936f1a5109b3b39
Uzair Sayyed <sayeduzair54321@gmail.com>
2024-04-24 18:00:49 +0530

    added some more sections in landing page

--- frontend/src/components/landingpage/HeroBanner.js
+++ frontend/src/components/landingpage/HeroBanner.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import heroBannerImg from "../../assets/heroBanner.png";
 
 function HeroBanner() {
   return (
-    <div>HeroBanner</div>
-  )
+    <section className='heroBanner-container'>
+      <div className='heroBanner-wrapper'>
+        <div className='heroBanner-left'>
+          <img src={heroBannerImg} />
+        </div>
+        <div className='heroBanner-right'>
+          <h1>
+            Student Report <span>Advisor</span>
+          </h1>
+          <p>Student Report Advisor is here to help with your academic journey. Elevate your grade with proper guidance, polished reports and Academic Excellence with the functionality "A Report with Recommendation".</p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default HeroBanner
+export default HeroBanner;
