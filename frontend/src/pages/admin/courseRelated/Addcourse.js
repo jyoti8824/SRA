cb4393dfba79e5b67d442eb22969892280f68286
Uzair Sayyed <sayeduzair54321@gmail.com>
2024-04-27 16:32:34 +0530

    chnages in add course form

--- frontend/src/pages/admin/courseRelated/Addcourse.js
+++ frontend/src/pages/admin/courseRelated/Addcourse.js
@@ -3,6 +3,18 @@ import { useNavigate } from "react-router-dom";
 import { CircularProgress } from "@mui/material";
 import Popup from "../../../components/Popup";
 import axios from "axios";
+import {
+  Grid,
+  Box,
+  Typography,
+  Paper,
+  Checkbox,
+  FormControlLabel,
+  TextField,
+  CssBaseline,
+  IconButton,
+  InputAdornment,
+} from "@mui/material";
 
 const AddCourse = () => {
   const navigate = useNavigate();
@@ -20,7 +32,7 @@ const AddCourse = () => {
 
   const submitHandler = async (event) => {
     event.preventDefault();
- 
+
     setLoader(true);
 
     try {
@@ -33,101 +45,115 @@ const AddCourse = () => {
         courselink,
       });
 
+      if (response.status >= 200 && response.status < 300) {
+        // Redirect user to admin home page
+        navigate("/");
 
-        if (response.status >= 200 && response.status < 300) {
-            // Redirect user to admin home page
-            navigate("/");
-            
-            // Reset form fields
-            setCourseTitle("");
-            setCourseDetails("");
-            setPrice("");
-            setPercentage("");
-            setCourseimagelink("");
-            setCourselink("");
-      
-            // Optionally, display a success message
-            setMessage("Course added successfully");
-          } else {
-            throw new Error("Network Error");
-          }
-        } catch (error) {
-          // Handle error
-          setMessage("An error occurred. Please try again.");
-        } finally {
-          // Reset loader state
-          setLoader(false);
-        }
+        // Reset form fields
+        setCourseTitle("");
+        setCourseDetails("");
+        setPrice("");
+        setPercentage("");
+        setCourseimagelink("");
+        setCourselink("");
+
+        // Optionally, display a success message
+        setMessage("Course added successfully");
+      } else {
+        throw new Error("Network Error");
+      }
+    } catch (error) {
+      // Handle error
+      setMessage("An error occurred. Please try again.");
+    } finally {
+      // Reset loader state
+      setLoader(false);
+    }
   };
 
   return (
     <>
       <div className='register'>
-        <form className='registerForm' onSubmit={submitHandler}>
-          <span className='registerTitle'>Add Course</span>
-          <label>Course Title</label>
-          <input
-            className='registerInput'
-            type='text'
-            placeholder='Enter course title...'
-            value={coursetitle}
-            onChange={(event) => setCourseTitle(event.target.value)}
-            required
-          />
+        <Box
+          sx={{
+            my: 10,
+            mx: "auto",
+            display: "flex",
+            flexDirection: "column",
+            p: 10,
+            width: "90%",
+            maxWidth: "600px",
+            alignItems: "center",
+            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+            borderRadius: "10px",
+          }}
+        >
+          <form className='registerForm' onSubmit={submitHandler}>
+            <span className='registerTitle'>Add Course</span>
+            <label>Course Title</label>
+            <input
+              className='registerInput'
+              type='text'
+              placeholder='Enter course title...'
+              value={coursetitle}
+              onChange={(event) => setCourseTitle(event.target.value)}
+              required
+            />
 
-          <label>Course Details</label>
-          <input
-            className='registerInput'
-            type='text'
-            placeholder='Enter Course details...'
-            value={coursedetails}
-            onChange={(event) => setCourseDetails(event.target.value)}
-            required
-          />
+            <label>Course Details</label>
+            <input
+              className='registerInput'
+              type='text'
+              placeholder='Enter Course details...'
+              value={coursedetails}
+              onChange={(event) => setCourseDetails(event.target.value)}
+              required
+            />
 
-          <label>Price</label>
-          <input
-            className='registerInput'
-            type='text'
-            placeholder='Enter course Price...'
-            value={price}
-            onChange={(event) => setPrice(event.target.value)}
-            required
-          />
-          <label>Percentage</label>
-          <input
-            className='registerInput'
-            type='number'
-            placeholder='Enter Percentage...'
-            value={percentage}
-            onChange={(event) => setPercentage(event.target.value)}
-            required
-          />
+            <label>Price</label>
+            <input
+              className='registerInput'
+              type='text'
+              placeholder='Enter course Price...'
+              value={price}
+              onChange={(event) => setPrice(event.target.value)}
+              required
+            />
+            <label>Percentage</label>
+            <input
+              className='registerInput'
+              type='number'
+              placeholder='Enter Percentage...'
+              value={percentage}
+              onChange={(event) => setPercentage(event.target.value)}
+              required
+            />
 
-          <label>Course Image Link</label>
-          <input
-            className='registerInput'
-            type='text'
-            placeholder='Enter Course Image link ...'
-            value={courseImageLink}
-            onChange={(event) => setCourseimagelink(event.target.value)}
-            required
-          />
+            <label>Course Image Link</label>
+            <input
+              className='registerInput'
+              type='text'
+              placeholder='Enter Course Image link ...'
+              value={courseImageLink}
+              onChange={(event) => setCourseimagelink(event.target.value)}
+              required
+            />
 
-          <label>Course Link</label>
-          <input
-            className='registerInput'
-            type='text'
-            placeholder='Enter Course Link...'
-            value={courselink}
-            onChange={(event) => setCourselink(event.target.value)}
-            required
-          />
+            <label>Course Link</label>
+            <input
+              className='registerInput'
+              type='text'
+              placeholder='Enter Course Link...'
+              value={courselink}
+              onChange={(event) => setCourselink(event.target.value)}
+              required
+            />
 
-          <button className='registerButton' type='submit' disabled={loader}>
-            {loader ? <CircularProgress size={24} color='inherit' /> : "Add"}
-          </button>
-        </form>
+            <button className='registerButton' type='submit' disabled={loader}>
+              {loader ? <CircularProgress size={24} color='inherit' /> : "Add"}
+            </button>
+          </form>
+        </Box>
       </div>
       <Popup
         message={message}
